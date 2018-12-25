2e96cf192fe505ab8ff680e697941a9839b414c2
anh.nqv <gdt.anv@gmail.com>
2018-12-25 09:19:15 +0700

    push code

--- Tutorial/Tutorial/Source/source.cpp
+++ Tutorial/Tutorial/Source/source.cpp
@@ -2,6 +2,7 @@
 #include "GLFW/glfw3.h"
 #include "glm/glm.hpp"
 
+#include "LoadShaders.h"
 
 #include <iostream>
 #include <string>
@@ -31,7 +32,9 @@ int main()
 		}
 
 		glfwMakeContextCurrent(window);
-		if (!glewInit())
+
+		glewExperimental = true;
+		if (GLEW_OK != glewInit())
 		{
 			std::cout << "failed to initialize glew\n";
 			throw int(1);
@@ -39,7 +42,7 @@ int main()
 
 		glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		glClearColor(0.0f, 0.4f, 0.4f, 0.0f);
 		glEnable(GL_DEPTH_TEST);
 		glDepthFunc(GL_LESS);
 
@@ -48,8 +51,30 @@ int main()
 		glGenVertexArrays(1, &vertex_id);
 		glBindVertexArray(vertex_id);
 
+		GLfloat vertices[6] = { -0.90f, -0.90f, 0.85f, -0.90f, -0.90f,  0.85f };
+		GLuint buffer_id;
+		glGenBuffers(1, &buffer_id);
+		glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		ShaderInfo  shaders[] =
+		{
+			{ GL_VERTEX_SHADER, "source/triangles.vert" },
+			{ GL_FRAGMENT_SHADER, "source/triangles.frag" },
+			{ GL_NONE, NULL }
+		};
+
+		GLuint program = LoadShaders(shaders);
+		glUseProgram(program);
+
+		glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, nullptr);
+		glEnableVertexAttribArray(0);
+
 		do {
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+			glBindVertexArray(vertex_id);
+			glDrawArrays(GL_TRIANGLES, 0, 3);
 
 			glfwSwapBuffers(window);
 			glfwPollEvents();
